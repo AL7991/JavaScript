a55616c2ab11e455bee976c0f16c9d718b4ca6ce
Litewka Albert <albis@vp.pl>
2023-03-05 18:10:19 +0100

    Adding redux and creating store, action, reducer to fetch logged user

---
+++ bank-app/src/actions/usersActions.js
@@ -0,0 +1,21 @@
+import { FETCH_LOGGED_USER_ERROR, FETCH_LOGGED_USER_INPROGRESS, FETCH_LOGGED_USER_SUCCESS } from "./actionsTypes";
+
+export const fetchLoggedUserInProgress = () =>{
+return {
+    type: FETCH_LOGGED_USER_INPROGRESS
+    };
+};
+
+export const fetchLoggedUserSuccess = LoggedUser =>{
+    return {
+        type: FETCH_LOGGED_USER_SUCCESS,
+        user: LoggedUser
+        };
+};
+
+export const fetchLoggedUserError = error =>{
+    return {
+        type: FETCH_LOGGED_USER_ERROR,
+        loggedUserError: error
+        };
+};
---
+++ bank-app/src/reducers/usersReducer.js
@@ -0,0 +1,34 @@
+import { FETCH_LOGGED_USER_ERROR, FETCH_LOGGED_USER_INPROGRESS, FETCH_LOGGED_USER_SUCCESS } from "../actions/actionsTypes";
+
+const initialState = {
+        inprogress: false,
+        error:null,
+        user:null
+}
+
+const userReducer = (state = initialState,action) => {
+    if(action.type === FETCH_LOGGED_USER_INPROGRESS){
+        return{
+            ...state,
+            inprogress: true
+        };
+    }
+    else if(action.type === FETCH_LOGGED_USER_SUCCESS){
+        return{
+            ...state,
+            inprogress: false,
+            user: action.user
+        };
+    }
+    else if(action.type === FETCH_LOGGED_USER_ERROR){
+        return{
+            ...state,
+            inprogress: false,
+            error: action.error
+        };
+    }
+
+return state;
+
+}
+export default userReducer;
