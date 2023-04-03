3a4f722d951e6745bd5d624fd0f94c71d7d60d10
Litewka Albert <albis@vp.pl>
2023-04-03 16:36:20 +0200

    Adding redux-persist and react-router-dom to project

--- bank-app/src/store/store.js
+++ bank-app/src/store/store.js
@@ -1,8 +1,21 @@
-import thunk from "redux-thunk";
 import { applyMiddleware, createStore } from "redux";
 import combineReducers from "../reducers/mainReducers";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage/session";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+
+const persistConfig = {
+    key:'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel2,
+    blacklist: []
+}
 
 const middlewares = [thunk];
-const store = createStore(combineReducers, applyMiddleware(...middlewares));
+const pReducer = persistReducer(persistConfig, combineReducers);
+const store = createStore(pReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
-export default store ;
+export { store };
+export { persistor };
