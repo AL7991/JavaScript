00a1720ec81a9331acf1b8db3ef0146f8a961fa5
Litewka Albert <albis@vp.pl>
2023-04-05 18:46:42 +0200

    adding response page for bad requests

---
+++ bank-app/src/pages/notFound/notFound.js
@@ -0,0 +1,14 @@
+import { Row, Col, Container } from "react-bootstrap";
+
+const NotFoundPage = () => {
+return(
+    <Container>
+        <Row>
+            <Col> Page does not exist. </Col>
+        </Row>
+    </Container>
+)
+
+}
+
+export {NotFoundPage};
